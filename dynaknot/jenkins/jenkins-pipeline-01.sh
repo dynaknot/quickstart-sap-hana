ccaa1392230b7db8ac811869c4c357a883c103a8
Lusztig Zoltan <zlusztig@gmail.com>
2021-04-11 18:42:29 +0200

    Update

--- dynaknot/jenkins/jenkins-pipeline-01.sh
+++
@@ -1,7 +0,0 @@
-
-# copy git files to S3 bucket
-aws s3 cp 's3://s3-eu-central-1.amazonaws.com/dynaknot/' . || error_exit 'Failed to download AWS CodeDeploy Agent.'
-
-# create immagebuilder pipeline
-quickstart-sap-hana/dynaknot/imagebuilder/create-pipeline-goldenami-ubuntu18.sh
-
